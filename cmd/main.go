7ad07b619b539f05c2f4b59a36ae923872639812
suzushin54 <mkm.snicr@gmail.com>
2024-04-22 21:54:30 +0900

    Fix implementation of CreateInventory to match gRPC interface requirements

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"github.com/suzushin54/actor-based-inventory/actors"
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"connectrpc.com/connect"
 	_ "github.com/oklog/ulid/v2"
+	"github.com/suzushin54/actor-based-inventory/actors"
 	inventoryv1 "github.com/suzushin54/actor-based-inventory/gen/inventory/v1"
 	"github.com/suzushin54/actor-based-inventory/gen/inventory/v1/inventoryv1connect"
 	"github.com/suzushin54/actor-based-inventory/service"
@@ -15,9 +16,12 @@ import (
 	"golang.org/x/net/http2/h2c"
 )
 
-const httpServerAddr = "localhost:8080"
+// const httpServerAddr = "localhost:8080"
+const httpServerAddr = "0.0.0.0:8080"
 
 func main() {
+	log.Printf("main process started\n")
+
 	brokerAddr := os.Getenv("KAFKA_BOOTSTRAP_SERVERS")
 	s, err := service.NewService(brokerAddr, "inventory")
 	if err != nil {
@@ -30,14 +34,15 @@ func main() {
 	path, handler := inventoryv1connect.NewInventoryServiceHandler(iss)
 	mux.Handle(path, handler)
 
+	log.Printf("HTTP server starting on %s", httpServerAddr)
 	if err := http.ListenAndServe(
 		httpServerAddr,
 		// Use h2c so we can serve HTTP/2 without TLS.
 		h2c.NewHandler(mux, &http2.Server{}),
 	); err != nil {
-		return
+		log.Fatalf("Failed to start HTTP server: %v", err)
 	}
-
+	log.Printf("HTTP server listening on %s", httpServerAddr)
 }
 
 // InventoryServiceServer implements the inventoryv1connect.InventoryServiceServer interface.
@@ -52,18 +57,21 @@ func NewInventoryServiceServer(s *service.Service) *InventoryServiceServer {
 	}
 }
 
-func (s *InventoryServiceServer) CreateInventoryItem(
-	r *inventoryv1.CreateInventoryRequest,
-) (*inventoryv1.CreateInventoryResponse, error) {
+func (s *InventoryServiceServer) CreateInventory(
+	ctx context.Context,
+	req *connect.Request[inventoryv1.CreateInventoryRequest],
+) (*connect.Response[inventoryv1.CreateInventoryResponse], error) {
+	log.Printf("CreateInventory: %v\n", req.Msg)
+
 	item := &actors.InventoryItem{
-		ID:    r.Inventory.ProductId,
-		Count: int(r.Inventory.Quantity),
+		ID:    req.Msg.Inventory.ProductId,
+		Count: int(req.Msg.Inventory.Quantity),
 	}
 
 	if err := s.service.AddInventoryItem(item); err != nil {
 		return nil, err
 	}
 
-	fmt.Printf("CreateInventoryItem: %v\n", r.Inventory)
-	return nil, nil
+	res := &inventoryv1.CreateInventoryResponse{}
+	return connect.NewResponse(res), nil
 }
